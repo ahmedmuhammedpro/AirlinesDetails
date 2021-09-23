5a1fbd9a0955262fab66cf3ac4880d57b5e51a37
ahmedmuhammed <ahmedmuhammedpro@gmail.com>
2021-09-23 03:34:14 +0200

    add network state listener

--- app/src/main/java/com/ahmed/airlinesdetails/main_view/MainActivity.kt
+++ app/src/main/java/com/ahmed/airlinesdetails/main_view/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ahmed.airlinesdetails.main_view
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,12 +13,16 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.ahmed.airlinesdetails.R
 import com.ahmed.airlinesdetails.databinding.ActivityMainBinding
+import com.ahmed.airlinesdetails.receivers.ConnectivityReceiver
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConnectivityReceiver.ConnectivityReceiverListener {
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var navController: NavController
     private lateinit var dataBinding: ActivityMainBinding
+    private var connectivityReceiver: ConnectivityReceiver? = null
+    private var isFirstTimeToReceiver = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +32,48 @@ class MainActivity : AppCompatActivity() {
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
-
         mainViewModel.navDestination.observe(this) {
             navController.navigate(it.destinationId, it.bundle)
         }
+
+        connectivityReceiver = ConnectivityReceiver()
+        registerReceiver(connectivityReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
     }
 
     fun setupToolbarTitle(title: String) {
         dataBinding.toolbar.findViewById<TextView>(R.id.toolbar_title).text = title
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(connectivityReceiver)
+        ConnectivityReceiver.connectivityReceiverListener = null
+    }
+
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        if (isConnected) {
+            if (isFirstTimeToReceiver) {
+                val snackBar = Snackbar.make(dataBinding.root, R.string.network_retrieve, Snackbar.LENGTH_LONG)
+                val snackBarTextView = snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+                snackBarTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_wifi_retrieved,0, 0, 0)
+                snackBarTextView.compoundDrawablePadding = 28
+                snackBar.show()
+            }
+
+            isFirstTimeToReceiver = true
+        } else {
+            val snackBar = Snackbar.make(dataBinding.root, R.string.network_lost, Snackbar.LENGTH_LONG)
+            val snackBarTextView = snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+            snackBarTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_wifi_lost,0, 0, 0)
+            snackBarTextView.compoundDrawablePadding = 28
+            snackBar.show()
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/ahmed/airlinesdetails/receivers/ConnectivityReceiver.kt
@@ -0,0 +1,27 @@
+package com.ahmed.airlinesdetails.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+
+class ConnectivityReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        connectivityReceiverListener?.onNetworkConnectionChanged(isConnected(context))
+    }
+
+    private fun isConnected(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return connectivityManager.activeNetworkInfo?.isConnected == true
+    }
+
+    interface ConnectivityReceiverListener {
+        fun onNetworkConnectionChanged(isConnected: Boolean)
+    }
+
+    companion object {
+        var connectivityReceiverListener: ConnectivityReceiverListener? = null
+    }
+
+}
